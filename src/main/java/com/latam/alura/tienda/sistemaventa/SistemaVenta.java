cb0e7111f66ba4453a0ae3b75d22abb2b23acd37
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-09-26 17:03:40 -0500

    Inicializacion del repositorio

---
+++ src/main/java/com/latam/alura/tienda/sistemaventa/SistemaVenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.latam.alura.tienda.sistemaventa;
+
+/**
+ *
+ * @author Home
+ */
+public class SistemaVenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
